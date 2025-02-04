f24a2d799b06a67e57e1735dcdad3ea7b77a1a76
Franziska Kiel <kiel.franziska@outlook.com>
2025-02-04 15:19:51 +0100

    refactor: better API interaction + error handling

--- script.js
+++ script.js
@@ -2,21 +2,31 @@ const btnQuote = document.querySelector("#btn-get-quote");
 
 btnQuote.addEventListener("click", renderQuote);
 
-function renderQuote(event) {
+async function renderQuote(event) {
   const pQuote = document.querySelector("#quote");
   const pAuthor = document.querySelector("#author");
 
   pQuote.innerText = "";
   pAuthor.innerText = "";
 
-  fetch("https://dummy-apis.netlify.app/api/quote")
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then((quote) => {
-      pQuote.append(document.createTextNode(quote.quote));
-      pAuthor.append(document.createTextNode(quote.author));
-    });
+  // Wait for fetch to complete -> usage of await requires renderQuote to be an async function
+  let randomQuoteObj = await fetchRandomQuote();
+
+  if (randomQuoteObj === null) {
+    pQuote.append(document.createTextNode("Quote could not be loaded."));
+    return;
+  }
+
+  pQuote.append(document.createTextNode(randomQuoteObj.quote));
+  pAuthor.append(document.createTextNode("— " + randomQuoteObj.author));
+}
+
+async function fetchRandomQuote() {
+  try {
+    const response = await fetch("https://dummy-apis.netlify.app/api/quote/");
+    return await response.json();
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
 }
